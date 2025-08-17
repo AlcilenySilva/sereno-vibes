015123b7cd358a109ce9da26478c64b9a52abf6b
AlcilenySilva <capic.2023118tads0045@aluno.ifpi.edu.br>
2025-08-17 02:56:22 -0300

    ajustes para hospedagem

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth"; 
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOtUX8oE31zE6vSPtYRyIsyjv_0wgyt6o",
+  authDomain: "sereno-vibes.firebaseapp.com",
+  projectId: "sereno-vibes",
+  storageBucket: "sereno-vibes.appspot.com", 
+  messagingSenderId: "963561678895",
+  appId: "1:963561678895:web:dfdd36ff0e47d2c548feda"
+};
+
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const auth = getAuth(app); 
+export const db = getFirestore(app);
