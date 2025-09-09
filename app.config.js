f8798ae8c07d902e791677fb5c3ffc7ae1a1f7b6
AlcilenySilva <capic.2023118tads0045@aluno.ifpi.edu.br>
2025-09-09 08:52:13 -0300

    id

--- app.config.js
+++ app.config.js
@@ -1,5 +1,6 @@
 export default ({ config }) => ({
   ...config,
+  slug: "sereno-vibes", 
   extra: {
     firebaseApiKey: process.env.FIREBASE_API_KEY,
     firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -8,7 +9,8 @@ export default ({ config }) => ({
     firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     firebaseAppId: process.env.FIREBASE_APP_ID,
     eas: {
-      projectId: "sereno-vibes" 
+      projectId: "f733a786-e0b0-4605-b491-9e95c57f86d9" 
     }
   },
 });
+
